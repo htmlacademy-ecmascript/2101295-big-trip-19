db10d9535048dd76abb8b5a6f9bd9418db65d4c7
Николай <rukin.nik.1996@mail.ru>
2023-02-04 12:39:14 +0700

    поправил сортировку

--- src/view/form-sort-view.js
+++ src/view/form-sort-view.js
@@ -5,28 +5,28 @@ function createSortFormTemplate(currentSortType) {
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
       <div class="trip-sort__item  trip-sort__item--day">
-        <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day"  ${currentSortType === SortType.DAY ? 'checked' : ''}>
-        <label class="trip-sort__btn" for="sort-day" data-sort-type="${SortType.DAY}">Day</label>
+        <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day"  ${currentSortType === SortType.DAY ? 'checked' : ''} data-sort-type="${SortType.DAY}">
+        <label class="trip-sort__btn" for="sort-day">Day</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--event">
         <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" disabled>
-        <label class="trip-sort__btn" for="sort-event" data-sort-type="${SortType.EVENT}">Event</label>
+        <label class="trip-sort__btn" for="sort-event">Event</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--time">
-        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" ${currentSortType === SortType.TIME ? 'checked' : ''}>
-        <label class="trip-sort__btn" for="sort-time" data-sort-type="${SortType.TIME}">Time</label>
+        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" ${currentSortType === SortType.TIME ? 'checked' : ''} data-sort-type="${SortType.TIME}">
+        <label class="trip-sort__btn" for="sort-time">Time</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--price">
-        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" ${currentSortType === SortType.PRICE ? 'checked' : ''}>
-        <label class="trip-sort__btn" for="sort-price" data-sort-type="${SortType.PRICE}">Price</label>
+        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" ${currentSortType === SortType.PRICE ? 'checked' : ''} data-sort-type="${SortType.PRICE}">
+        <label class="trip-sort__btn" for="sort-price">Price</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--offer">
         <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer" disabled>
-        <label class="trip-sort__btn" for="sort-offer" data-sort-type="${SortType.OFFERS}">Offers</label>
+        <label class="trip-sort__btn" for="sort-offer">Offers</label>
       </div>
     </form>`
   );
@@ -49,10 +49,10 @@ export default class FormrSortView extends AbstractView {
   }
 
   #sortTypeChangeHandler = (evt) => {
-    const filterELement = evt.target.closest('.trip-sort__item').querySelector('.trip-sort__input');
-    if (filterELement) {
-      evt.preventDefault();
-      this.#handleSortTypeChange(evt.target.dataset.sortType);
+    if(evt.target.tagName !== 'INPUT'){
+      return;
     }
+    evt.preventDefault();
+    this.#handleSortTypeChange(evt.target.dataset.sortType);
   };
 }
